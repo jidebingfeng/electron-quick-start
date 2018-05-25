b8f526f93fe6659490b5070a5245c19ce1fffb93
zlong <jidebingfeng@126.com>
2018-05-25 17:42:33 +0800

    成功读取文件

--- main.js
+++ main.js
@@ -11,43 +11,36 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600,frame: false,fullscreen:true,fullscreenWindowTitle:true})
-    mainWindow.webContents.openDevTools()
-  console.log("createWindow")
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({zoomToPageWidth: true})
+    // mainWindow.maximize()
 
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
 
 // 在主进程中.
-const {ipcMain} = require('electron')
-ipcMain.on('asynchronous-message', (event, arg) => {
-    console.log(arg) // prints "ping"
-event.sender.send('asynchronous-reply', 'pong')
-})
+    const {ipcMain} = require('electron')
+    ipcMain.on('openDevTools', function (event, args) {
+        mainWindow.webContents.openDevTools()
+    })
 
-ipcMain.on('synchronous-message', (event, arg) => {
-    console.log(arg) // prints "ping"
-event.returnValue = 'pong'
-})
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -56,19 +49,19 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
